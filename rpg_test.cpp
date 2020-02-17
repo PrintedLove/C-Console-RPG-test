5e5539839839388c6e60cdc1371b1bd5962fbd1c
PrintedLove <PrintedLove@github.com>
2020-02-17 12:58:41 +0900

    2020.02.17

--- rpg_test.cpp
+++ rpg_test.cpp
@@ -9,61 +9,66 @@
 #define TRUE 1
 #define MAP_X_MAX 96
 #define MAP_Y_MAX 32
-#define FLOOR_Y 22
+#define FLOOR_Y 26
 
 typedef struct _Character {
-    short x = MAP_X_MAX / 2 + 1, y = MAP_Y_MAX / 2;
-    float x_accel = 0, y_accel = 0;
-    short size_x = 3, size_y = 3;
+    short coord[2] = {MAP_X_MAX / 2, MAP_Y_MAX / 2};
+    float accel[2] = {0, };
+    short size[2] = {3, 3};
     float flyTime = 0;
     bool direction = TRUE;	//true=right, false=left
     			//character stat
     char name[16];
     int lv = 1;
-    unsigned int exp = 0, expi = 100;
-    int hpm = 100, hp = hpm;
-    int mpm = 50, mp = mpm;
+    unsigned int exp[2] = {100, 0};	//0=expi, 1=exp
+    int hp[2] = {100, 0};	//0=hpm, 1=hp
+    int mp[2] = {50, 0};
     int power = 10;
     short weapon = 0;
     unsigned int score = 0;
     			//animation control
-    short leg_m = 1, atk_m[3] = {FALSE, 0, 1};	//motion
-    unsigned int gen_tick, leg_tick = 0, atk_tick = 0, dash_tick;
+    short motion[4] = {1, FALSE, 0, 1};	//motion	//leg_motion, attack_motion 1, 2, 3
+    unsigned int tick[4] = {0, };	//gen_tick, leg_tick, atk_tick, dash_tick
 }Character;
 
 typedef struct _Enemy {
-    short x, y;
-    float x_accel = 0, y_accel = 0;
-    short size_x, size_y;
+    short coord[2];
+    float accel[2];
+    short size[2];
     float flyTime = 0;
     bool direction = TRUE;
     			//enemy stat
     bool alive = FALSE;
     short kinds;
-    int hpm, hp;
+    int hp[2];
     int dam;
+    			//animation control
+    short motion[3];	//motion
+    unsigned int tick[4];
 }Enemy;
 
 Character character;
 Enemy enemy;
 
 void StartGame();
-void UpdateGame(unsigned int sistem_tick);
+void UpdateGame();
 void SetConsole();
-void ControlUI(unsigned int sistem_tick);
-void ControlCharacter(unsigned int sistem_tick);
+void ControlUI();
+void ControlCharacter();
+void ControlEnemy();
 
-void MotionControl(short *x, short *y, float *x_accel, float *y_accel, short size_x, short size_y, float *flyTime);
-void DrawBox(int x, int y, int size_x, int size_y);
-void DrawNumber(int x, int y, int num);
-void DrawSprite(int x, int y, int size_x, int size_y, char spr[]);
+void MotionControl(short coord[], float accel[], short size[], float *flyTime);
+void DrawBox(short x, short y, short size_x, short size_y);
+void DrawNumber(short x, short y, int num);
+void DrawSprite(short x, short y, short size_x, short size_y, char spr[]);
 void FillMap(char str[], char str_s, int max_value);
-void EditMap(int x, int y, char str);
+void EditMap(short x, short y, char str);
 
 int NumLen(int num);
 int StrLen(char str[]);
 
 short stat_weapon[] = {5, 10, 15};
+unsigned int tick = GetTickCount();
 
 char sprite_floor[MAP_X_MAX];
 char sprite_character[10] = " 0  | _^_";
@@ -83,13 +88,11 @@ char mapData[MAP_X_MAX * MAP_Y_MAX];	//array for graphics
 int main() {
 	StartGame();
 	
-	unsigned int system_tick = GetTickCount();
-	
 	while (TRUE) {
-		if (system_tick + 30 < GetTickCount()) {
-			system_tick = GetTickCount();
+		if (tick + 30 < GetTickCount()) {
+			tick = GetTickCount();
 			
-			UpdateGame(system_tick);
+			UpdateGame();
 		}
 	}
 	
@@ -105,15 +108,15 @@ void StartGame() {
 	FillMap(sprite_floor, '=', MAP_X_MAX);
 }
 
-void UpdateGame(unsigned int sistem_tick) {
-	unsigned int tick = sistem_tick;
+void UpdateGame() {
 	
 	FillMap(mapData, ' ', MAP_X_MAX * MAP_Y_MAX);	//initialize mapdata
 	
-	ControlUI(tick);
-	ControlCharacter(tick);
+	ControlUI();
+	ControlCharacter();
+	ControlEnemy();
 	
-	printf("%s", mapData);	//update mapdata
+	fputs(mapData, stdout);	//update mapdata
 }
 
 void SetConsole() {
@@ -131,9 +134,8 @@ void SetConsole() {
     srand((unsigned int)time(NULL));
 }
 
-void ControlUI(unsigned int sistem_tick) {
-	unsigned int tick = sistem_tick;
-	int expPer = roundf(character.exp * 100 / character.expi);
+void ControlUI() {
+	int expPer = roundf(character.exp[1] * 100 / character.exp[0]);
 	int len;	//length of previous sprite
 	
 	DrawSprite(1, FLOOR_Y, MAP_X_MAX, 1, sprite_floor);	//draw floor
@@ -158,157 +160,165 @@ void ControlUI(unsigned int sistem_tick) {
 	DrawNumber(MAP_X_MAX - NumLen(character.score), 2, character.score);
 	
 	DrawSprite(4, 4, 3, 1, "HP:");	//draw HP
-	DrawNumber(8, 4, character.hp);
-	EditMap(9 + NumLen(character.hp), 4, '/');
-	DrawNumber(11 + NumLen(character.hp), 4, character.hpm);
+	DrawNumber(8, 4, character.hp[1]);
+	EditMap(9 + NumLen(character.hp[1]), 4, '/');
+	DrawNumber(11 + NumLen(character.hp[1]), 4, character.hp[0]);
 	
 	DrawSprite(4, 5, 3, 1, "MP:");	//draw MP
-	DrawNumber(8, 5, character.mp);
-	EditMap(9 + NumLen(character.mp), 5, '/');
-	DrawNumber(11 + NumLen(character.mp), 5, character.mpm);
+	DrawNumber(8, 5, character.mp[1]);
+	EditMap(9 + NumLen(character.mp[1]), 5, '/');
+	DrawNumber(11 + NumLen(character.mp[1]), 5, character.mp[0]);
 	
 	DrawSprite(4, 7, 6, 1, "Power:");	//draw power
 	DrawNumber(11, 7, character.power);
 }
 
-void ControlCharacter(unsigned int sistem_tick) {
-	unsigned int tick = sistem_tick;
+void ControlCharacter() {
 	bool move = FALSE, attack = FALSE;
 	
-	if (character.exp >= character.expi) {	//LV up
-		character.lv ++; character.hpm += 10; character.mpm += 5; character.power ++;
-		character.exp = 0; character.expi += character.lv * 10;
+	if (character.exp[1] >= character.exp[0]) {	//LV up
+		character.lv ++; character.hp[0] += 10; character.mp[0] += 5; character.power ++;
+		character.exp[1] = 0; character.exp[0] += character.lv * 10;
 	}
 	
-	if (character.gen_tick + 900 < tick) {	//hp, mp
-		character.gen_tick = tick;
-		character.hp += roundf(character.hpm * 0.01);
-		character.mp += roundf(character.mpm * 0.05);
+	if (character.tick[0] + 900 < tick) {	//hp, mp
+		character.tick[0] = tick;
+		character.hp[1] += roundf(character.hp[0] * 0.01);
+		character.mp[1] += roundf(character.mp[0] * 0.05);
 	}
-	if (character.hp > character.hpm) character.hp = character.hpm;
-	if (character.mp > character.mpm) character.mp = character.mpm;
+	if (character.hp[1] > character.hp[0])
+		character.hp[1] = character.hp[0];
+	if (character.mp[1] > character.mp[0])
+		character.mp[1] = character.mp[0];
 	
-	if (GetAsyncKeyState(0x5A) & 0x8000 && character.flyTime == 0) {	//keyboard
+	if (GetAsyncKeyState(0x5A) & 0x8000 && character.flyTime == 0) {	//attack
 		attack = TRUE;
-		character.atk_m[0] = TRUE;
+		character.motion[1] = TRUE;
 	}
-	if (character.atk_m[0]) {
-		if (tick > character.atk_tick + 150) {	//attack motion calculation
-			character.atk_tick = tick;
-			character.atk_m[1]++;
+	if (character.motion[1]) {
+		if (tick > character.tick[2] + 150) {	//attack motion calculation
+			character.tick[2] = tick;
+			character.motion[2]++;
 		}
 		
-		if (character.atk_m[1] > 3) {
+		if (character.motion[2] > 3) {
 			if (attack) {
-				character.atk_m[1] = 1; character.atk_m[2]++;
+				character.motion[2] = 1; character.motion[3]++;
 			} else {
-				character.atk_m[0] = FALSE; character.atk_m[1] = 0; character.atk_m[2] = 1;
+				character.motion[1] = FALSE; character.motion[2] = 0; character.motion[3] = 1;
 			}
 			
-			if (character.atk_m[2] > 3)
-				character.atk_m[2] = 1;	
+			if (character.motion[3] > 3)
+				character.motion[3] = 1;	
 		}
 	} else {
-		if (GetAsyncKeyState(VK_LEFT) & 0x8000 && character.x > 1) {	//move left
-			if (character.x_accel > -1)
-				character.x_accel = -1;
+		if (GetAsyncKeyState(VK_LEFT) & 0x8000 && character.coord[0] > 1) {	//move left
+			if (character.accel[0] > -1)
+				character.accel[0] = -1;
+				
 			character.direction = FALSE;
 			move = TRUE;
 		}
 		
-		if (GetAsyncKeyState(VK_RIGHT) & 0x8000 && character.x < MAP_X_MAX - 2) {	//move right
-			if (character.x_accel < 1)
-				character.x_accel = 1;
+		if (GetAsyncKeyState(VK_RIGHT) & 0x8000 && character.coord[0] < MAP_X_MAX - 2) {	//move right
+			if (character.accel[0] < 1)
+				character.accel[0] = 1;
+				
 			character.direction = TRUE;
 			move = TRUE;
 		}
 			
-		if (GetAsyncKeyState(0x58) & 0x8000 && character.dash_tick + 1200 <= tick) {	//dash
-			character.x_accel = character.direction * 8 - 4;
-			character.dash_tick = tick;
+		if (GetAsyncKeyState(0x58) & 0x8000 && character.tick[3] + 1200 <= tick) {	//dash
+			character.accel[0] = character.direction * 8 - 4;
+			character.tick[3] = tick;
 		}
 	}
 	
-	if (GetAsyncKeyState(VK_UP) & 0x8000 && character.y + 3 == FLOOR_Y)	//jump
-			character.y_accel = -1.75;
+	if (GetAsyncKeyState(VK_UP) & 0x8000 && character.coord[1] + 3 == FLOOR_Y)	//jump
+			character.accel[1] = -1.75;
 	
-	MotionControl(&character.x, &character.y, &character.x_accel, &character.y_accel, character.size_x, character.size_y, &character.flyTime);
+	MotionControl(character.coord, character.accel, character.size, &character.flyTime);
 	
-	if (tick > character.leg_tick + 90) {	//leg tick	
-		character.leg_tick = tick;
+	if (tick > character.tick[1] + 90) {	//leg tick	
+		character.tick[1] = tick;
 		
 		if (move == TRUE)
-			character.leg_m++;
+			character.motion[0]++;
 		else
-			character.leg_m = 0;
+			character.motion[0] = 0;
 			 
-		if (character.leg_m > 3)
-			character.leg_m = 1;
+		if (character.motion[0] > 3)
+			character.motion[0] = 1;
 	}
 	
-	DrawSprite(character.x, character.y, character.size_x, character.size_y, sprite_character);	//draw character sprite
+	DrawSprite(character.coord[0], character.coord[1], character.size[0], character.size[1], sprite_character);	//draw character sprite
 	
 	if (character.direction) {
-		EditMap(character.x, character.y + 1, '(');
+		EditMap(character.coord[0], character.coord[1] + 1, '(');
 	} else {
-		EditMap(character.x + 2, character.y + 1, ')');
+		EditMap(character.coord[0] + 2, character.coord[1] + 1, ')');
 	}
 	
-	if (character.x_accel > 1)
-		DrawSprite(character.x - 3, character.y, 1, 3, "===");
-	if (character.x_accel < -1)
-		DrawSprite(character.x + 5, character.y, 1, 3, "===");
+	if (character.accel[0] > 1)
+		DrawSprite(character.coord[0] - 3, character.coord[1], 1, 3, "===");
+	if (character.accel[0] < -1)
+		DrawSprite(character.coord[0] + 5, character.coord[1], 1, 3, "===");
 		
-	if (character.atk_m[0] && character.atk_m[1] > 0) {	//draw attack motion
-		if (character.atk_m[2] == 3) {
-			DrawSprite(character.x - 5 + 8 * character.direction, character.y, 5, 3, sprite_normalAttack[character.direction][character.atk_m[1] - 1]);
+	if (character.motion[1] && character.motion[2] > 0) {	//draw attack motion
+		if (character.motion[3] == 3) {
+			DrawSprite(character.coord[0] - 5 + 8 * character.direction, character.coord[1], 5, 3, sprite_normalAttack[character.direction][character.motion[2] - 1]);
 		} else {
-			if (character.atk_m[1] == 2) {
-				EditMap(character.x - 2 + 6 * character.direction, character.y + 1, 'o');
-				DrawSprite(character.x - 5 + 10 * character.direction, character.y + 1, 3, 1, sprite_Weapon[character.direction][character.weapon]);
+			if (character.motion[2] == 2) {
+				EditMap(character.coord[0] - 2 + 6 * character.direction, character.coord[1] + 1, 'o');
+				DrawSprite(character.coord[0] - 5 + 10 * character.direction, character.coord[1] + 1, 3, 1, sprite_Weapon[character.direction][character.weapon]);
 			} else {
-				EditMap(character.x + 2 * character.direction, character.y + 1, 'o');
-				DrawSprite(character.x - 3 + 6 * character.direction, character.y + 1, 3, 1, sprite_Weapon[character.direction][character.weapon]);
+				EditMap(character.coord[0] + 2 * character.direction, character.coord[1] + 1, 'o');
+				DrawSprite(character.coord[0] - 3 + 6 * character.direction, character.coord[1] + 1, 3, 1, sprite_Weapon[character.direction][character.weapon]);
 			}
 		}
 	} else {
-		EditMap(character.x + character.direction * 2, character.y + 1, 'o');
-		DrawSprite(character.x - 3 + 6 * character.direction, character.y + 1, 3, 1, sprite_Weapon[character.direction][character.weapon]);
+		EditMap(character.coord[0] + character.direction * 2, character.coord[1] + 1, 'o');
+		DrawSprite(character.coord[0] - 3 + 6 * character.direction, character.coord[1] + 1, 3, 1, sprite_Weapon[character.direction][character.weapon]);
 		
-		if (character.leg_m == 3)
-			EditMap(character.x + 1, character.y + 1, 'l');
+		if (character.motion[0] == 3)
+			EditMap(character.coord[0] + 1, character.coord[1]+ 1, 'l');
 	}
 	
 	if (move)
-		DrawSprite(character.x, character.y + 2, 3, 1, sprite_character_leg[character.direction][character.leg_m - 1]);	//draw leg motion
+		DrawSprite(character.coord[0], character.coord[1] + 2, 3, 1, sprite_character_leg[character.direction][character.motion[0] - 1]);	//draw leg motion
 }
 
-void MotionControl(short *x, short *y, float *x_accel, float *y_accel, short size_x, short size_y, float *flyTime) {	// Object motion control
-	float x_value = *x_accel, y_value = *y_accel;
+void ControlEnemy() {
+	bool move = FALSE, attack = FALSE;
 	
-	if (*y + size_y < FLOOR_Y) {
+}
+
+void MotionControl(short coord[], float accel[], short size[], float *flyTime) {	// Object motion control
+	float x_value = accel[0], y_value = accel[1];
+	
+	if (coord[1] + size[1] < FLOOR_Y) {
 		*flyTime += 0.05;
-		*y_accel += *flyTime;
+		accel[1] += *flyTime;
 	} else {
 		*flyTime = 0;
 	}
 	
 	if (x_value != 0 || y_value != 0) {
-		if (*x + x_value < 1)
-			x_value = 1 - *x;
-		if (*x + size_x + x_value > MAP_X_MAX)
-			x_value = MAP_X_MAX - size_x - *x;
-		if (*y + size_y + y_value > FLOOR_Y)
-			y_value = FLOOR_Y - *y - size_y;
+		if (coord[0] + x_value < 1)
+			x_value = 1 - coord[0];
+		if (coord[0] + size[0] + x_value > MAP_X_MAX)
+			x_value = MAP_X_MAX - size[0] - coord[0];
+		if (coord[1] + size[1] + y_value > FLOOR_Y)
+			y_value = FLOOR_Y - coord[1] - size[1];
 	}
 	
-	*x += floor(x_value + 0.5); *y += floor(y_value + 0.5);
+	coord[0] += floor(x_value + 0.5); coord[1] += floor(y_value + 0.5);
 	
-	if (*x_accel > 0) *x_accel -= 0.5; if (*x_accel < 0) *x_accel += 0.5;
-	if (*y_accel > 0) *y_accel -= 0.1; if (*y_accel < 0) *y_accel += 0.1;
+	if (accel[0] > 0) accel[0] -= 0.5; if (accel[0] < 0) accel[0] += 0.5;
+	if (accel[1] > 0) accel[1] -= 0.1; if (accel[1] < 0) accel[1] += 0.1;
 }
 
-void DrawBox(int x, int y, int size_x, int size_y) {	//draw box of size_x, size_y at x, y coordinates
+void DrawBox(short x, short y, short size_x, short size_y) {	//draw box of size_x, size_y at x, y coordinates
 	EditMap(x, y, '.'); EditMap(x + size_x - 1, y, '.');
 	EditMap(x, y + size_y - 1, '\''); EditMap(x + size_x - 1, y + size_y - 1, '\'');
 	
@@ -321,7 +331,7 @@ void DrawBox(int x, int y, int size_x, int size_y) {	//draw box of size_x, size_
 	}
 }
 
-void DrawNumber(int x, int y, int num) {	//draw numbers at x, y coordinates (align left)
+void DrawNumber(short x, short y, int num) {	//draw numbers at x, y coordinates (align left)
 	int tmp = num;
 	short len = NumLen(tmp), cnt = len;
     char str[len];
@@ -335,7 +345,7 @@ void DrawNumber(int x, int y, int num) {	//draw numbers at x, y coordinates (ali
     DrawSprite(x, y, len, 1, str);
 }
 
-void DrawSprite(int x, int y, int size_x, int size_y, char spr[]) {	//draw sprite of size_x, size_y at x, y coordinates
+void DrawSprite(short x, short y, short size_x, short size_y, char spr[]) {	//draw sprite of size_x, size_y at x, y coordinates
 	for (int i = 0; i < size_y; i++) {
 		for (int n = 0; n < size_x; n++)
 			EditMap(x + n, y + i, spr[i * size_x + n]);
@@ -347,7 +357,7 @@ void FillMap(char str[], char str_s, int max_value) {	//array initialization
 		str[i] = str_s;
 }
 
-void EditMap(int x, int y, char str) {	// edit x, y coordinate mapdata
+void EditMap(short x, short y, char str) {	// edit x, y coordinate mapdata
 	if (x > 0 && y > 0 && x - 1 < MAP_X_MAX && y - 1 < MAP_Y_MAX)
 		mapData[(y - 1) * MAP_X_MAX + x - 1] = str;
 }
